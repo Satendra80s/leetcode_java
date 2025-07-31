a6a415cb070ed32d9fe91700d5c8c2514597a676
Satendra Patel <satendrapatel270@gmail.com>
2025-07-31 22:56:32 +0530

    Add files via upload

---
+++ SubarrayBitwise.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class SubarrayBitwise {
+    public int subarrayBitwiseORs(int[] arr) {
+        List<Integer> s = new ArrayList<>();
+        int l=0;
+
+        for(final int a:arr){
+            final int r =s.size();
+            s.add(a);
+            for(int i = l;i<r; ++i)
+                if(s.get(s.size()-1)!= (s.get(i) | a))
+                    s.add(s.get(i)| a);
+            l = r;
+        }
+        return new HashSet<>(s).size();
+        
+    }
+    public static void main(String[] args) {
+        SubarrayBitwise solutation= new SubarrayBitwise ();
+        int [] arr={1,1,2};
+        System.out.println(""+solutation.subarrayBitwiseORs(arr));
+    }
+}
