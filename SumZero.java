f864cb3c470a86be239f1dd499f146802d5da20b
Satendra Patel <satendrapatel270@gmail.com>
2025-09-07 20:51:16 +0530

    Add files via upload

---
+++ SumZero.java
@@ -0,0 +1,12 @@
+public class SumZero {
+
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+
+        for(int i=0; i < n; i++)
+        ans[i] = i * 2 -n + 1;
+
+        return ans;
+    }
+
+}
